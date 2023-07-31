6e84b785f18b2398a60ac5f89ea424126035b85f
Nishant <nishantsingh95340@gmail.com>
2023-07-31 15:21:47 +0530

    adding redux toolkit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 import LivePerformance from "@/components/home/LivePerformance";
 import Main from "@/components/home/Main";
-import Navbar from "@/components/home/Navbar";
 import Performance from "@/components/home/Performance";
 import Reviews from "@/components/home/Reviews";
-import Image from "next/image";
 
 export default function Home() {
   return (
